1d2cc19254516b18f4c7c3cb2170d8935afddc41
reciliencia_clothes3 <83990977+lucasDuet@users.noreply.github.com>
2021-11-09 00:04:10 -0300

    imgenes pasnado
    no lo termine desp sigo

---
+++ js/prueba.js
@@ -0,0 +1,16 @@
+var imagenes =['imagenes/serpiente.png','imagenes/logo.png']
+cont=0;
+function pasar(div3img1){
+    div3img1.addEventlistener('click',e=>){
+    let atras =pasar.querySelector('.atras'),
+    adelante =pasar.querySelector('.adelante'),
+    img= pasar.querySelector('imagenes')
+    tgt=e.target;
+ }
+if(tgt== atras){
+    
+}
+
+
+}
+
